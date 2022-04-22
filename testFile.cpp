2787ea7abf7ba1577b382ade5582eceeeee4021d
Alisa Lazareva <volunteer.alisa@gmail.com>
2022-04-22 18:04:12 -0500

    wrong syntax

--- testFile.cpp
+++ testFile.cpp
@@ -10,7 +10,7 @@ int main()
     cout<<"FileName: "<<endl;
     cin>>fileName;
     userFile.open(fileName);
-    cout<<str(userFile.is_open());
+    cout<<userFile.is_open();
     string line;
     if(userFile.is_open())
     {
